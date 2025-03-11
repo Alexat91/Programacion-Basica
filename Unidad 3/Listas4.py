e5c09199b7d1ad25e11c119faf945a4b46eb5da2
Alexat91 <rodriguezkarolalexa28@gmail.com>
2025-03-11 07:58:10 -0600

    Create Listas4.py
    Crear Listas

---
+++ Unidad 3/Listas4.py
@@ -0,0 +1,25 @@
+
+# Lista de cadenas
+Nombres = ["Lupita", "Aaron", "Alexa", "Manuel", "Saba","Victor", "Cristian", "Oswaldo", "Enrique", "Roberto"]
+Edades = [18, 18, 18, 18, 19, 18, 18, 18, 18, 18]
+Libre  = ["Perros", "Gatos", "Pajaros", "Urones", "Luciernagas", "Anejorros", "Pandas", "Koalas", "Zebra", "Oso"]
+materias = ["Calculo", "Algebra", "Estadistica", "Contabilidad","Programacion", "Materiales", "Ingles"]
+Profesores = ["Adilene", "Guilleromo", "Marlem", "Laura", "Eduardo","Enrique"]
+
+vacia = []
+print(Nombres[0])
+print(Edades[0])
+print(Libre[0])
+print(materias [0])
+print(Profesores [0])
+print(len(Nombres))
+print(max(Edades))
+print(min(Edades))
+Nombres.sort()
+print(Nombres)
+Edades.sort()
+print(Edades)
+materias.remove("Contabilidad")
+print(materias)
+materias.append("Programacion Avanzada")
+print(materias)
