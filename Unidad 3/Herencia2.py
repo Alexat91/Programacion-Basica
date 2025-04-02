069ebae9213ae0be24d1ff782d1f08bb1bc56825
Alexat91 <rodriguezkarolalexa28@gmail.com>
2025-04-02 07:59:48 -0600

    Update Herencia2.py
    .

--- Unidad 3/Herencia2.py
+++ Unidad 3/Herencia2.py
@@ -1,38 +1,40 @@
 # Herencia
 # Clase base o padre
-class Persona:
-    def __init__(self, nombre, edad):
+class Cantantes:
+    def __init__(self, nombre, genero):
         self.nombre = nombre
-        self.edad = edad
+        self.genero = genero
 
     def presentarse(self):
-        return f"Hola, me llamo {self.nombre} y tengo {self.edad} años."
+        return f"Hola, me llamo {self.nombre} y canto música del genero {self.genero} ."
+
+
+class Discos(Discos):
+    def __init__(self, nombre, precio, cantante):
+        super().__init__(nombre, precio, cantante)
+        self.precio = precio
+
+    def presentarse(self):
+        return f"Este album es de  {self.nombre}, se llama {self.tituloa} y cuesta  {self.precio}."
+
 
 # Clase derivada o hija
-class Estudiante(Persona):
-    def __init__(self, nombre, edad, grado):
-        super().__init__(nombre, edad)  # Llamada al constructor de la clase padre
-        self.grado = grado
+class Cancion(Cancion):
+    def __init__(self, nombre, titulo, tiempo):
+        super().__init__(nombre, tiempo, titulo)  # Llamada al constructor de la clase padre
+        self.tiempo = tiempo
 
     def presentarse(self):
         # Sobrescribimos el método de la clase padre
-        return f"Hola, me llamo {self.nombre}, tengo {self.edad} años y estudio en {self.grado}."
+        return f"La  cancion es de  {self.nombre}, dura {self.tiempo} y el titulo es {self.titulo}."
 
-# Otra clase derivada
-class Profesor(Persona):
-    def __init__(self, nombre, edad, materia):
-        super().__init__(nombre, edad)
-        self.materia = materia
-
-    def presentarse(self):
-        return f"Hola, soy el profesor {self.nombre}, tengo {self.edad} años y enseño {self.materia}."
 
 # Programa principal
 if __name__ == "__main__":
-    persona = Persona("Carlos", 40)
-    estudiante = Estudiante("Ana", 20, "3er año de Ingeniería")
-    profesor = Profesor("Luis", 50, "Matemáticas")
+    cantantes = Cantantes("Jenni Rivera", "Regional Mexicano")
+    discos = Discos("Jenni RIvera", "X", "$300")
+    cancion = Cancion("Jenni Rivera", "2.45 min", "X")
 
-    print(persona.presentarse())
-    print(estudiante.presentarse())
-    print(profesor.presentarse())
+    print(cantantes.presentarse())
+    print(discos.presentarse())
+    print(cancion.presentarse())
